402914fe7e6671d8a2934084ec52f47e8cbd5423
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-09 11:04:13 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+# require'pry'
+ array = ["xbox", "ps5", "nintendo", "comcast", "facebook"]
+
+def my_each(array) # put argument(s) here
+  index = 0
+  while index < array.length
+    # binding.pry
+    yield(array[index])
+    index = index+1
+  end
+
+  return array 
+
+end
+
+
+my_each(array) do |i|
+  # binding.pry
+  puts i 
+
 end
